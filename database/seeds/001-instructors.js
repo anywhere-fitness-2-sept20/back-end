8055050c200d7e47a20fb2a5de0d6bb9051967b3
Josh Riddle <riddlej9879@yahoo.com>
2020-09-23 20:50:08 -0500

    Username no-longer case sensitive. Database updated

--- database/seeds/001-instructors.js
+++ database/seeds/001-instructors.js
@@ -4,13 +4,13 @@ exports.seed = async function (knex) {
   await knex("instructors").insert([
     {
       name: "Charles",
-      username: "ExistingInstructor1",
+      username: "existinginstructor1",
       password: await bcryptjs.hash("instructorPass1", 2),
       role: "instructor",
     },
     {
       name: "Jane",
-      username: "ExistingInstructor2",
+      username: "existinginstructor2",
       password: await bcryptjs.hash("instructorPass2", 2),
       role: "instructor",
     },
--- database/seeds/002-clients.js
+++ database/seeds/002-clients.js
@@ -4,37 +4,37 @@ exports.seed = async function (knex) {
   await knex("clients").insert([
     {
       name: "John Doe",
-      username: "ExistingClient1",
+      username: "existingclient1",
       password: await bcryptjs.hash("ClientPass1", 2),
       role: "client",
     },
     {
       name: "Jane Doe",
-      username: "ExistingClient2",
+      username: "existingclient2",
       password: await bcryptjs.hash("ClientPass2", 2),
       role: "client",
     },
     {
       name: "Billy Batson",
-      username: "ExistingClient3",
+      username: "existingclient3",
       password: await bcryptjs.hash("ClientPass3", 2),
       role: "client",
     },
     {
       name: "Bruce Whine",
-      username: "ExistingClient4",
+      username: "existingclient4",
       password: await bcryptjs.hash("ClientPass4", 2),
       role: "client",
     },
     {
       name: "Johnny Doe",
-      username: "ExistingClient5",
+      username: "existingclient5",
       password: await bcryptjs.hash("ClientPass5", 2),
       role: "client",
     },
     {
       name: "Anonymous Person",
-      username: "ExistingClient6",
+      username: "existingclient6",
       password: await bcryptjs.hash("ClientPass6", 2),
       role: "client",
     },
